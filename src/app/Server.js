0d06235a2c01484fbb72e83ed54835ae921e325b
Sai Karthik <sai.karthik@in.ey.com>
2026-02-16 13:57:54 +0530

    bind issue

--- src/app/Server.js
+++ src/app/Server.js
@@ -17,6 +17,6 @@ app.get(/.*/, (req, res) => {
 // Railway provides PORT automatically
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
